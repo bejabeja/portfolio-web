86de11e3f96af124c2f965a4e008b83628b3e384
bejabeja <miriam.abella211@gmail.com>
2026-06-10 16:13:34 +0200

    remove cursor

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import styles from "./App.module.css";
 import { useEffect } from "react";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
-import CustomCursor from "./components/CustomCursor/CustomCursor";
 import Experience from "./components/Experience/Experience";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
@@ -64,7 +63,6 @@ const ScrollAnimations = () => {
 
 const AppRoutes = () => (
   <div className={styles.App}>
-    <CustomCursor />
     <Navbar />
     <ScrollToHash />
     <ScrollAnimations />
